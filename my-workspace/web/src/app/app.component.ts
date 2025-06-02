875cac72d27c4fb086539fa1bd6b3d4a775d719f
uvais498 <muhammed.uvais@outlook.com>
2025-06-02 15:21:36 +0300

    bootstrap setup

--- my-workspace/web/src/app/app.component.ts
+++ my-workspace/web/src/app/app.component.ts
@@ -6,6 +6,8 @@ import { selectIsAuthenticated } from '@my-workspace/shared';
 import { UserActions, SupabaseclientService } from '@my-workspace/shared';
 import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
+
+
 @Component({
   imports: [NxWelcomeComponent, RouterModule,CommonModule],
   selector: 'app-root',
@@ -14,7 +16,7 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   title = 'web';
-isAuthenticated$!: Observable<boolean>;
+  isAuthenticated$!: Observable<boolean>;
   constructor(
     private s: SupabaseclientService,
     
@@ -22,6 +24,7 @@ isAuthenticated$!: Observable<boolean>;
   ) {}
 
   ngOnInit(): void {
+    this.setCompanyTheme('arabianoud');
     this.isAuthenticated$ = this.store.select(selectIsAuthenticated);
     console.log(this.isAuthenticated$);
     this.login();
@@ -35,4 +38,39 @@ isAuthenticated$!: Observable<boolean>;
 
 
   }
+
+
+
+setCompanyTheme(company: 'arabianoud' | 'oudelite' | 'rosewood') {
+  const themes: { [key in 'arabianoud' | 'oudelite' | 'rosewood']: { primary: string } } = {
+    arabianoud: {
+      primary: '#873D1D',
+    },
+    oudelite: {
+      primary: '#000000',
+    },
+    rosewood: {
+      primary: '#8d8734',
+    }
+  };
+
+  const theme = themes[company];
+
+  if (theme) {
+    document.documentElement.style.setProperty('--bs-primary', theme.primary);
+
+    // Set --bs-primary-rgb (used in borders, shadows, etc.)
+    const rgb = this.hexToRgb(theme.primary);
+    document.documentElement.style.setProperty('--bs-primary-rgb', rgb);
+  }
+}
+
+hexToRgb(hex: string): string {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+  return `${r}, ${g}, ${b}`;
+}
+
+
 }
